3a7a15322164c221589141e5985715d825b062ae
niloy143 <niloymahmudapu@gmail.com>
2022-08-17 12:07:38 +0600

    error fix and commentry

--- bank-hisab/another.js
+++ bank-hisab/another.js
@@ -1,34 +1,57 @@
+// 1. Validation and prevention from typing wrong inputs (Now they can't type except number values)
+const depoWith = document.getElementsByClassName('depo-with');
+
+for(const element of depoWith) {
+    element.addEventListener('input', function(){
+        const fullValue = element.value;
+        const lastChar = fullValue.length - 1;
+
+        if(isNaN(fullValue[lastChar])){
+            element.value = fullValue.slice(0, lastChar);
+        }
+    })
+}
+
+// 2. all the function to be called when nedded
+
+// function to get the number from string value
 function getValue(idName) {
     const theElement = document.getElementById(idName);
     const theValue = parseFloat(theElement.value);
     theElement.value = '';
-    if(!isNaN(theValue) && theValue > 0) {
-        return theValue;
-    }
-    else {
-        return 0;
-    }
+    return theValue;
 }
 
+// function to get the number from string innerText
 function getInnerText(idName) {
     const theElement = document.getElementById(idName);
     const theInnerText = parseFloat(theElement.innerText);
     return theInnerText;
 }
 
-document.getElementById('depo-btn').addEventListener('click', function() {
+// function to set different innerText inside element
+function setInnerText(idName, theText) {
+    return document.getElementById(idName).innerText = theText;
+
+}
+
+// 3. event listeners to be listened
+
+// deposit button click event
+document.getElementById('depo-btn').addEventListener('click', function () {
     const depoInput = getValue('depo-input');
     const deposit = getInnerText('deposit');
     const balance = getInnerText('balance');
-    document.getElementById('deposit').innerText = depoInput + deposit;
-    document.getElementById('balance').innerText = depoInput + balance;
+    setInnerText('deposit', (deposit + depoInput));
+    setInnerText('balance', (balance + depoInput));
 })
 
-document.getElementById('with-btn').addEventListener('click', function() {
+// withdraw button click event
+document.getElementById('with-btn').addEventListener('click', function () {
     const withInput = getValue('with-input');
     const withdraw = getInnerText('withdraw');
     const balance = getInnerText('balance');
-    if(withInput > balance) {return};
-    document.getElementById('withdraw').innerText = withInput + withdraw;
-    document.getElementById('balance').innerText = balance - withInput;
+    if (withInput > balance) { return };
+    setInnerText('withdraw', (withdraw + withInput));
+    setInnerText('balance', (balance - withInput));
 })
